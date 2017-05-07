9ca1b0e71a1578f242139146db50ef281b488bd4
devmetal91 <devmetal91@gmail.com>
2017-05-07 15:48:03 +0200

    Added semantic, jquery, and start redesign post component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Post from './modules/Post';
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <Post />
-          <Feed />
+      <div className="App">
+        <div className="ui two column centered grid">
+          <div className="two column row">
+            <div className="column">
+              <Post />
+            </div>
+          </div>
+          <div className="two column row">
+            <div className="column">
+              <Feed />
+            </div>
+          </div>
         </div>
+      </div>
     );
   }
 }
--- src/modules/Post/components/Post.jsx
+++ src/modules/Post/components/Post.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const UiHeader = () => (
+  <h4 className="ui header">
+    <i className="write icon"></i>
+    <div className="content">
+      Write something!
+  </div>
+  </h4>
+);
+
 export default class extends Component {
   static propTypes = {
     onSend: PropTypes.func.isRequired,
@@ -26,15 +35,31 @@ export default class extends Component {
     onSend(value);
   }
 
-  error = () => this.setState({error: true});
-  done = () => this.setState({done: true, value: ''});
+  error = () => this.setState({ error: true });
+  done = () => this.setState({ done: true, value: '' });
 
   render() {
     const { value, error, done } = this.state;
+    const placeholder = 'Write a message or dnd a file';
     return (
-      <div className="post">
-        <textarea value={value} onChange={this.handleChange}></textarea>
-        <button onClick={this.handleSubmit}>Fire Up</button>
+      <div className="post ui raised segment">
+        <UiHeader />
+        <div className="ui form">
+          <div className="field">
+            <textarea
+              rows="3"
+              value={value}
+              placeholder={placeholder}
+              onChange={this.handleChange}>
+            </textarea>
+          </div>
+          <button
+            onClick={this.handleSubmit}
+            className="ui primary button">
+            <i className="icon send"></i>
+            Fire Up
+        </button>
+        </div>
         {error && <span className='error'>You have to write something!</span>}
         {done && <span className='done'>Your message sent!</span>}
       </div>
